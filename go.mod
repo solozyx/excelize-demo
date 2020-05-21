d4a0775254ecea2ac1e92d39143d49f9b3d3b4bb
solozyx <zhaoyingxin@aliyun.com>
2020-05-21 18:06:12 +0800

    demo

---
+++ go.mod
@@ -0,0 +1,8 @@
+module excelize-demo
+
+go 1.14
+
+require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1
+	github.com/sirupsen/logrus v1.6.0
+)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/sirupsen/logrus"
+)
+
+func init() {
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
+
+func main() {
+	openFile()
+}
+
+func openFile() {
+	f, err := excelize.OpenFile("Book1.xlsx")
+	if err != nil {
+		panic(err)
+	}
+
+	sheetMap := f.GetSheetMap()
+	logrus.Debugf("GetSheetMap sheetMap=%+v", sheetMap)
+
+	for mK, mV := range sheetMap {
+		logrus.Debugf("(%T) %+v : (%T) %+v", mK, mK, mV, mV)
+	}
+
+	logrus.Debugf("sheet1 %s", sheetMap[1])
+	strs := f.GetRows(sheetMap[1])
+	for i := 0; i < len(strs); i++ {
+		logrus.Debugf("strs[%d] len=%d datas=%+v", i, len(strs[i]), strs[i])
+		for j := 0; j < len(strs[i]); j++ {
+			if strs[i][j] == "" {
+				continue
+			}
+			logrus.Debugf("strs[row_%d][col_%d] type=%T data=%+v", i, j, strs[i][j], strs[i][j])
+		}
+	}
+
+	logrus.Debugf("sheet2 %s", sheetMap[2])
+	strs = f.GetRows(sheetMap[2])
+	for i := 0; i < len(strs); i++ {
+		logrus.Debugf("strs[%d] len=%d datas=%+v", i, len(strs[i]), strs[i])
+		for j := 0; j < len(strs[i]); j++ {
+			if strs[i][j] == "" {
+				continue
+			}
+			logrus.Debugf("strs[row_%d][col_%d] type=%T data=%+v", i, j, strs[i][j], strs[i][j])
+		}
+	}
+}
