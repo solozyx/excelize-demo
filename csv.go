ffcc6ccffe58d31dd26b0daf8b4da09db27b958a
solozyx <zhaoyingxin@aliyun.com>
2020-05-22 11:13:13 +0800

    xlsxImport

--- csv.go
+++ csv.go
@@ -98,3 +98,31 @@ func csvExportAppend() {
 		log.Fatalf("can not write all, err is %+v", err)
 	}
 }
+
+func csvExport() {
+	records := [][]string{
+		{"first_name", "last_name", "username"},
+		{"Rob", "Pike", "rob"},
+		{"Ken", "Thompson", "ken"},
+		{"Robert", "Griesemer", "gri"},
+	}
+
+	f, err := os.OpenFile("Book1.csv", os.O_RDWR, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	w := csv.NewWriter(f)
+
+	for _, record := range records {
+		if err := w.Write(record); err != nil {
+			log.Fatalln("error writing record to csv:", err)
+		}
+	}
+
+	// Write any buffered data to the underlying writer (standard output).
+	w.Flush()
+
+	if err := w.Error(); err != nil {
+		log.Fatal(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"encoding/csv"
-	"log"
-	"os"
-
 	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/sirupsen/logrus"
 )
@@ -18,17 +14,11 @@ func main() {
 	// csvImportLarge()
 	// csvImportSmall()
 	// csvExportAppend()
+
+	xlsxImport()
 }
 
-func openCsv() {
-	f, err := excelize.OpenFile("Book1.csv")
-	if err != nil {
-		panic(err)
-	}
-	logrus.Debugf("f=%+v", f)
-}
-
-func openXlsx() {
+func xlsxImport() {
 	f, err := excelize.OpenFile("Book1.xlsx")
 	if err != nil {
 		panic(err)
@@ -41,71 +31,22 @@ func openXlsx() {
 		logrus.Debugf("(%T) %+v : (%T) %+v", mK, mK, mV, mV)
 	}
 
-	logrus.Debugf("sheet1 %s", sheetMap[1])
-	strs := f.GetRows(sheetMap[1])
-	for i := 0; i < len(strs); i++ {
-		logrus.Debugf("strs[%d] len=%d datas=%+v", i, len(strs[i]), strs[i])
-		for j := 0; j < len(strs[i]); j++ {
-			if strs[i][j] == "" {
-				continue
+	for sheetIndex := 1; sheetIndex <= len(sheetMap); sheetIndex++ {
+		logrus.Debugf("sheet[%d] %s", sheetIndex, sheetMap[sheetIndex])
+		strs := f.GetRows(sheetMap[sheetIndex])
+
+		if len(strs) == 0 {
+			continue
+		}
+
+		for i := 0; i < len(strs); i++ {
+			logrus.Debugf("strs[%d] len=%d datas=%+v", i, len(strs[i]), strs[i])
+			for j := 0; j < len(strs[i]); j++ {
+				if strs[i][j] == "" {
+					continue
+				}
+				logrus.Debugf("strs[row_%d][col_%d] type=%T data=%+v", i, j, strs[i][j], strs[i][j])
 			}
-			logrus.Debugf("strs[row_%d][col_%d] type=%T data=%+v", i, j, strs[i][j], strs[i][j])
 		}
 	}
-
-	logrus.Debugf("sheet2 %s", sheetMap[2])
-	strs = f.GetRows(sheetMap[2])
-	for i := 0; i < len(strs); i++ {
-		logrus.Debugf("strs[%d] len=%d datas=%+v", i, len(strs[i]), strs[i])
-		for j := 0; j < len(strs[i]); j++ {
-			if strs[i][j] == "" {
-				continue
-			}
-			logrus.Debugf("strs[row_%d][col_%d] type=%T data=%+v", i, j, strs[i][j], strs[i][j])
-		}
-	}
-}
-
-func exportCsv() {
-	records := [][]string{
-		{"first_name", "last_name", "username"},
-		{"Rob", "Pike", "rob"},
-		{"Ken", "Thompson", "ken"},
-		{"Robert", "Griesemer", "gri"},
-	}
-
-	f, err := os.OpenFile("Book1.csv", os.O_RDWR, os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-	w := csv.NewWriter(f)
-
-	for _, record := range records {
-		if err := w.Write(record); err != nil {
-			log.Fatalln("error writing record to csv:", err)
-		}
-	}
-
-	// Write any buffered data to the underlying writer (standard output).
-	w.Flush()
-
-	if err := w.Error(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func importCsv() {
-	f, err := os.OpenFile("Book1.csv", os.O_RDWR, os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-	r := csv.NewReader(f)
-
-	strs, err := r.Read()
-	if err != nil {
-		panic(err)
-	}
-
-	logrus.Debugf("importCsv strs=%+v", strs)
-
 }
